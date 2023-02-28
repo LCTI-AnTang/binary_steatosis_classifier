00c96180daec6640edece62686e5a449adfa845b
Pedro Vianna <51378392+viannapedro@users.noreply.github.com>
2023-02-28 13:11:03 -0500

    Update arch_builder.py

--- arch_builder.py
+++ arch_builder.py
@@ -9,6 +9,13 @@ from keras.layers import Dropout
 from keras.models import Model
 
 def import_arch(architecture, in_shape, transfer_learning, classes):
+    '''
+    architecture: a string indicating the pre-trained architecture to use.
+    in_shape: a tuple representing the input shape of the data.
+    transfer_learning: a string indicating the file path of pre-trained weights to use for transfer learning (optional).
+    classes: an integer representing the number of output classes.
+    '''
+    
     K.set_image_data_format('channels_last')
     if architecture=='InceptionV3':
         model = InceptionV3(include_top=True, weights=None, input_shape = in_shape, classes=classes)
@@ -20,6 +27,7 @@ def import_arch(architecture, in_shape, transfer_learning, classes):
         model = VGG16(include_top=True, weights=None, input_shape = in_shape, classes=classes)
     
     if architecture=='VGG16-dropout':
+        # Adding two dropout layers before prediction
         model = VGG16(include_top=True, weights=None, input_shape = in_shape, classes=classes)
         fc1 = model.layers[-3]
         fc2 = model.layers[-2]
@@ -33,6 +41,7 @@ def import_arch(architecture, in_shape, transfer_learning, classes):
         model = Model(input=model.input, output=predictors)
         
     if transfer_learning:
+        # Importing trained weights
         K.set_image_data_format('channels_first')
         model.load_weights(transfer_learning,by_name=True)
         print('Transfer learning weights were loaded to the model.')
