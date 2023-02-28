edc1722e834f7a8ef039ac877ff2590f209929d7
Pedro Vianna <51378392+viannapedro@users.noreply.github.com>
2023-02-28 13:14:54 -0500

    Update saveimgs.py

--- saveimgs.py
+++ saveimgs.py
@@ -4,10 +4,20 @@
 import matplotlib.pyplot as plt
 
 def plot_acc_loss(training_metrics, validation_metrics, name):
+    """
+    This function plots the training and validation accuracy and loss for a given model during training. The function takes three arguments:
+
+    training_metrics: a tuple containing the training accuracy and loss for each epoch.
+    validation_metrics: a tuple containing the validation accuracy and loss for each epoch.
+    name: a string that will be used as part of the filename when saving the plots.
+    """
+    
+    # Defining markers to plot and output directory
     colours = ['b','r']
     markers = ['o','v'] 
     path_to_save = '/output/'
 
+    # Plotting training
     tr_acc = training_metrics[0]
     tr_loss = training_metrics[1]
     plt.plot(tr_acc, colours[0]+markers[0],label='Acc')
@@ -21,6 +31,7 @@ def plot_acc_loss(training_metrics, validation_metrics, name):
     plt.savefig(path_to_save+name+'_Training.png', dpi=300)
     plt.clf()
     
+    # Plotting validation
     vl_acc = validation_metrics[0]
     vl_loss = validation_metrics[1]
     plt.plot(vl_acc, colours[0]+markers[0],label='Acc')
@@ -34,6 +45,7 @@ def plot_acc_loss(training_metrics, validation_metrics, name):
     plt.savefig(path_to_save+name+'_Validation.png', dpi=300)
     plt.close()
     
+    # Plotting both on a single figure
     plt.plot(tr_acc, colours[0]+markers[0],label='Train - Acc')
     plt.plot(tr_loss, colours[1]+markers[0], label='Train - Loss')
     plt.plot(vl_acc, colours[0]+markers[1],label='Val - Acc')
